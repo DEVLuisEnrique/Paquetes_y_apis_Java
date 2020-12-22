9089f0a1fd72eecd12f3b8fb4c731a429a209051
Luis Enrique Rodriguez <luisenriquerodgon@gmail.com>
2020-12-22 10:23:52 -0400

    clase Math

---
+++ trabajar_con_clases_del_api/src/packageApi/Main.java
@@ -0,0 +1,14 @@
+package packageApi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.max(500, 200));
+		
+		
+	}
+
+}
